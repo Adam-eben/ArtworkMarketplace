425d2e9188827b18aa7dcde28e9f27e4e1be5dce
FarzeenKist <farzeenkistnareddy@gmail.com>
2022-06-30 23:06:51 +0400

    fixed minor bugs, added functionalities and redame file

--- artNFT.sol
+++ artNFT.sol
@@ -4,86 +4,138 @@ pragma solidity ^0.8.0;
 import "./artNFTtoken.sol";
 
 contract ArtMarketplace {
-  ArtToken private token;
+    ArtToken private token;
 
-  struct ItemForSale {
-    uint256 id;
-    uint256 tokenId;
-    address payable seller;
-    uint256 price;
-    bool isSold;
-  }
+    struct ItemForSale {
+        uint256 id;
+        uint256 tokenId;
+        address payable seller;
+        uint256 price;
+        bool isSold;
+    }
+    uint256 items;
 
-  ItemForSale[] public itemsForSale;
-  mapping(uint256 => bool) public activeItems; // tokenId => ativo?
+    mapping(uint256 => ItemForSale) itemsForSale;
+    mapping(uint256 => bool) public activeItems; // tokenId => bool onSale
 
-  event itemAddedForSale(uint256 id, uint256 tokenId, uint256 price);
-  event itemSold(uint256 id, address buyer, uint256 price);
+    event itemAddedForSale(uint256 id, uint256 tokenId, uint256 price);
+    event itemSold(uint256 id, address buyer, uint256 price);
 
-  constructor(ArtToken _token) {
-      token = _token;
-  }
-
-  modifier OnlyItemOwner(uint256 tokenId){
-    require(token.ownerOf(tokenId) == msg.sender, "Sender does not own the item");
-    _;
-  }
-
-  modifier HasTransferApproval(uint256 tokenId){
-    require(token.getApproved(tokenId) == address(this), "Market is not approved");
-    _;
-  }
-
-  modifier ItemExists(uint256 id){
-    require(id < itemsForSale.length && itemsForSale[id].id == id, "Could not find item");
-    _;
-  }
-
-  modifier IsForSale(uint256 id){
-    require(!itemsForSale[id].isSold, "Item is already sold");
-    _;
-  }
-
-  function putItemForSale(uint256 tokenId, uint256 price) 
-    OnlyItemOwner(tokenId) 
-    HasTransferApproval(tokenId) 
-    external 
-    returns (uint256){
-      require(!activeItems[tokenId], "Item is already up for sale");
-
-      uint256 newItemId = itemsForSale.length;
-      itemsForSale.push(ItemForSale({
-        id: newItemId,
-        tokenId: tokenId,
-        seller: payable(msg.sender),
-        price: price,
-        isSold: false
-      }));
-      activeItems[tokenId] = true;
-
-      assert(itemsForSale[newItemId].id == newItemId);
-      emit itemAddedForSale(newItemId, tokenId, price);
-      return newItemId;
-  }
-
-  function buyItem(uint256 id) 
-    ItemExists(id)
-    IsForSale(id)
-    HasTransferApproval(itemsForSale[id].tokenId)
-    payable 
-    external {
-      require(msg.value >= itemsForSale[id].price, "Not enough funds sent");
-      require(msg.sender != itemsForSale[id].seller);
-
-      itemsForSale[id].isSold = true;
-      activeItems[itemsForSale[id].tokenId] = false;
-      token.safeTransferFrom(itemsForSale[id].seller, msg.sender, itemsForSale[id].tokenId);
-      itemsForSale[id].seller.transfer(msg.value);
-
-      emit itemSold(id, msg.sender, itemsForSale[id].price);
+    constructor(ArtToken _token) {
+        token = _token;
     }
 
-  function totalItemsForSale() external view returns(uint256) {
-    return itemsForSale.length;
-  }
+    modifier OnlyItemOwner(uint256 tokenId) {
+        require(
+            token.ownerOf(tokenId) == msg.sender,
+            "Sender does not own the item"
+        );
+        _;
+    }
+
+    modifier HasTransferApproval(uint256 tokenId) {
+        require(
+            token.getApproved(tokenId) == address(this),
+            "Market is not approved"
+        );
+        _;
+    }
+
+    modifier ItemExists(uint256 id) {
+        require(
+            id < items && itemsForSale[id].id == id,
+            "Could not find item"
+        );
+        _;
+    }
+
+    modifier IsForSale(uint256 id) {
+        require(!itemsForSale[id].isSold, "Item is already sold");
+        _;
+    }
+
+    function putItemForSale(uint256 tokenId, uint256 price)
+        external
+        OnlyItemOwner(tokenId)
+        HasTransferApproval(tokenId)
+        returns (uint256)
+    {
+        require(!activeItems[tokenId], "Token is already up for sale");
+        require(price > 0, "Invalid price");
+        uint id = items;
+        items++;
+        itemsForSale[items] = ItemForSale(
+            id,
+            tokenId,
+            payable(msg.sender),
+            price,
+            false
+        );
+        activeItems[tokenId] = true;
+
+        require(itemsForSale[items].id == id, "Failed to create item");
+        
+        emit itemAddedForSale(id, tokenId, price);
+        return id;
+    }
+
+    function buyItem(uint256 id)
+        external
+        payable
+        ItemExists(id)
+        IsForSale(id)
+        HasTransferApproval(itemsForSale[id].tokenId)
+    {
+        require(msg.value == itemsForSale[id].price, "Not enough funds sent");
+        require(
+            msg.sender != itemsForSale[id].seller,
+            "Seller can't buy his own item"
+        );
+
+        itemsForSale[id].isSold = true;
+        address payable seller = itemsForSale[id].seller;
+        itemsForSale[id].seller = payable(msg.sender);
+        activeItems[itemsForSale[id].tokenId] = false;
+        token.safeTransferFrom(
+            itemsForSale[id].seller,
+            msg.sender,
+            itemsForSale[id].tokenId
+        );
+
+        (bool success, ) = seller.call{value: msg.value}("");
+        require(success, "Payment failed");
+
+        emit itemSold(id, msg.sender, itemsForSale[id].price);
+    }
+
+    // removes an item from sale
+    function removeItemFromSale(uint256 id)
+        external
+        OnlyItemOwner(itemsForSale[id].tokenId)
+        ItemExists(id)
+        IsForSale(id)
+    {
+        uint256 tokenId = itemsForSale[id].tokenId;
+        require(activeItems[tokenId], "Token isn't up for sale");
+        activeItems[tokenId] = false;
+        token.approve(address(0), tokenId); // approval is removed for marketplace
+    }
+
+    // relist an existing item with updated price to be on sale
+    function relistItem(uint256 id, uint256 _price)
+        public
+        ItemExists(id)
+        OnlyItemOwner(itemsForSale[id].tokenId)
+        HasTransferApproval(itemsForSale[id].tokenId)
+    {
+        uint256 tokenId = itemsForSale[id].tokenId;
+        require(!activeItems[tokenId], "Item is already up for sale");
+        require(_price > 0, "Invalid price");
+        activeItems[tokenId] = true;
+        itemsForSale[id].price = _price;
+    }
+
+    function totalItemsForSale() public view returns (uint256) {
+        return items;
+    }
 }
--- artNFTtoken.sol
+++ artNFTtoken.sol
@@ -1,50 +1,85 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract ArtToken is ERC721Enumerable{
-  using Counters for Counters.Counter;
+contract ArtToken is ERC721, ERC721Enumerable, ERC721URIStorage {
+    using Counters for Counters.Counter;
 
-  Counters.Counter private _tokenIds;
-  address public marketplace;
+    Counters.Counter private _tokenIds;
 
-  struct Item {
-    uint256 id;
-    address creator;
-    string uri;//metadata url
-  }
+    address public marketplace;
+    address contractOwner;
 
-  mapping(uint256 => Item) public Items; //id => Item
+    struct Item {
+        uint256 id;
+        address creator;
+        string uri; //metadata url
+    }
 
-  constructor () ERC721("ArtToken", "ARTK") {}
+    mapping(uint256 => Item) public Items; //id => Item
 
-  function mint(string memory uri) public returns (uint256){
-    _tokenIds.increment();
-    uint256 newItemId = _tokenIds.current();
-    _safeMint(msg.sender, newItemId);
-    approve(marketplace, newItemId);
+    constructor() ERC721("ArtToken", "ARTK") {
+        contractOwner = msg.sender;
+    }
 
-    Items[newItemId] = Item({
-      id: newItemId, 
-      creator: msg.sender,
-      uri: uri
-    });
+    function mint(string memory uri) public returns (uint256) {
+        require(bytes(uri).length > 7, "Invalid uri"); // uri using ipfs starts with ipfs://
+        uint256 newItemId = _tokenIds.current();
+        _tokenIds.increment();
+        _safeMint(msg.sender, newItemId);
+        approve(marketplace, newItemId);
 
-    return newItemId;
-  }
+        Items[newItemId] = Item({id: newItemId, creator: msg.sender, uri: uri});
 
-  function tokenURI(uint256 tokenId) public view override returns (string memory) {
-    require(_exists(tokenId), "ERC721URIStorage: URI query for nonexistent token");
-    return Items[tokenId].uri;
-  }
+        return newItemId;
+    }
 
-  function setMarketplace(address market) public {
-    //require(msg.sender ==);
-    marketplace = market;
-  }
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        require(
+            _exists(tokenId),
+            "ERC721URIStorage: URI query for nonexistent token"
+        );
+        return Items[tokenId].uri;
+    }
 
+    function setMarketplace(address market) public {
+        require(msg.sender == contractOwner, "Unauthorized user");
+        require(market != address(0), "Invalid address");
+        marketplace = market;
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _beforeTokenTransfer(
+        address from, 
+        address to,
+        uint256 tokenId
+    ) internal override(ERC721, ERC721Enumerable) {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function _burn(uint256 tokenId)
+        internal
+        override(ERC721, ERC721URIStorage)
+    {
+        super._burn(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
 }
